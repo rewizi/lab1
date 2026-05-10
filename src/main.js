eb2f3bd16537edcd2e80c30afa0b1c408ac2df0e
rewizi <antonivanton1995@gmail.com>
2026-05-10 11:20:57 +0300

    add posthog reverse proxy

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import posthog from 'posthog-js'
+
+posthog.init('phc_sRwm7FuJ5EeRoRGYxi9p7DFsESuYzTaRkUixZvgj7oZ8', {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2026-01-30',
+  person_profiles: 'identified_only',
+})
 
 createApp(App).mount('#app')
